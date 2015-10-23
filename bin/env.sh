2b2e0f16529a13f451c4f44a33a03fdae2d5e0b4
Nick Langridge <nicklangridge@hotmail.com>
2015-10-23 11:36:49 +0100

    Add comment about lib paths

--- bin/env.sh
+++ bin/env.sh
@@ -1,7 +1,6 @@
-#ROOT='/ensembl/restapi/current'
-#PORT=8080
-ROOT='/nfs/gns/homes/nickl/dev/vb-rest-2015-12'
-PORT=9360
+ROOT='/ensembl/restapi/current'
+PORT=8080
 export ENSEMBL_REST_SERVER_ROOT=$ROOT
 export ENSEMBL_REST_SERVER_PORT=$PORT
+# note eg-rest/lib is NOT in the path as we don't want it (at least for now)
 export PERL5LIB=$ROOT/ensembl/modules-io:$ROOT/ensembl/modules:$ROOT/ensembl-compara/modules:$ROOT/ensembl-variation/modules:$ROOT/ensembl-funcgen/modules:$ROOT/vectorbase-rest/lib:$ROOT/ensembl-rest/lib:$ROOT/ensemblgenomes-api/modules:$ROOT/ensembl-io/modules:/nfs/public/rw/ensembl/bioperl-live:/nfs/public/rw/ensembl/bioperl-extra:/nfs/public/web-hx/tools/vcftools/perl:/nfs/public/rw/ensembl/tools/tabix:/nfs/public/rw/ensembl/tools/tabix/perl:/nfs/public/rw/ensembl/tools/tabix/perl/lib/site_perl/5.16.3/x86_64-linux:/net/isilonP/public/rw/homes/ens_adm/src/lib/site_perl/5.16.3/x86_64-linux
