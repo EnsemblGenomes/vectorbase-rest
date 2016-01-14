9e97394690acc5ac64820e0f136111d1fee17cd3
Nick Langridge <nicklangridge@hotmail.com>
2016-01-14 13:03:31 +0000

    Config updates

--- bin/install.sh
+++ bin/install.sh
@@ -7,58 +7,57 @@ if [ $ver2 ]; then EG_BRANCH="$ver2"; fi
 
 ## Check out *Ensembl* code (API, web and (web) tools) from GitHub:
 for repo in \
-    ensembl \
-    ensembl-compara \
-    ensembl-funcgen \
-    ensembl-tools \
-    ensembl-variation \
-    ensembl-rest \
-    ensembl-io;
+  ensembl \
+  ensembl-compara \
+  ensembl-funcgen \
+  ensembl-tools \
+  ensembl-variation \
+  ensembl-rest \
+  ensembl-io;
 do
-    if [ ! -d "$repo" ]; then
-        echo "Checking out $repo (branch ${ENSEMBL_BRANCH})"
-        git clone --branch ${ENSEMBL_BRANCH} https://github.com/Ensembl/${repo}
-    else
-        echo Already got $repo, attempting to pull...
-        cd $repo
-        git pull
-        git status
-        cd ../
-    fi
+  if [ ! -d "$repo" ]; then
+    echo "Checking out $repo (branch ${ENSEMBL_BRANCH})"
+    git clone --branch ${ENSEMBL_BRANCH} https://github.com/Ensembl/${repo}
+  else
+    echo Already got $repo, attempting to pull...
+    cd $repo
+    git pull
+    git status
+    cd ../
+  fi
 
-    echo
-    echo
+  echo
+  echo
 done
 
 ## Check out *Ensembl Genomes* code (API, web and (web) tools) from GitHub:
 for repo in \
-    ensemblgenomes-api;
+  ensemblgenomes-api;
 do
-    if [ ! -d "$repo" ]; then
-        echo "Checking out $repo (branch ${EG_BRANCH})"
-        git clone --branch ${EG_BRANCH} https://github.com/EnsemblGenomes/${repo}
-    else
-        echo Already got $repo, attempting to pull...
-        cd $repo
-        git pull
-        git status
-        cd ../
-    fi
+  if [ ! -d "$repo" ]; then
+    echo "Checking out $repo (branch ${EG_BRANCH})"
+    git clone --branch ${EG_BRANCH} https://github.com/EnsemblGenomes/${repo}
+  else
+    echo Already got $repo, attempting to pull...
+    cd $repo
+    git pull
+    git status
+    cd ../
+  fi
 
-    echo
-    echo
+  echo
+  echo
 done
-
 
 ## Dir for starman logs and pid file
 mkdir logs
 
+## compile variation code
+(cd ensembl-variation/C_code && make)
+
 ## Copy default VB configs
 cp -vi vectorbase-rest/vectorbase_rest.conf.default vectorbase-rest/vectorbase_rest.conf
 cp -vi vectorbase-rest/bin/env.sh.default           vectorbase-rest/bin/env.sh
-
-## Copy static files
-cp -rv vectorbase-rest/root/static ensembl-rest/root
 
 ## Remove Ensembl versions of endpoints we fully override
 rm -v ensembl-rest/root/documentation/compara.conf
@@ -71,8 +70,3 @@ rm -v ensembl-rest/root/documentation/gavariant.conf
 rm -v ensembl-rest/root/documentation/gavariantset.conf
 rm -v ensembl-rest/root/documentation/gacallset.conf
 rm -v ensembl-rest/root/documentation/vep.conf
-
-## Apply /rest prefix
-cp -v vectorbase-rest/root/wrapper.tt             ensembl-rest/root/wrapper.tt
-cp -v vectorbase-rest/root/documentation/index.tt ensembl-rest/root/documentation/index.tt
-cp -v vectorbase-rest/root/documentation/info.tt  ensembl-rest/root/documentation/info.tt
