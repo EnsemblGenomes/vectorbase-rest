14c2f19f6a908ef2490f1c444f7c6e432b2b0027
Nick Langridge <nicklangridge@hotmail.com>
2018-01-16 14:58:17 +0000

    Port EG performance improvements

--- bin/server_control.pl
+++ bin/server_control.pl
@@ -9,7 +9,8 @@ my $root_dir   = $ENV{ENSEMBL_REST_ROOT} || "$Bin/../../";
 my $psgi_file  = "$root_dir/vectorbase-rest/vectorbase_rest.psgi";
 my $starman    = $ENV{ENSEMBL_REST_STARMAN} || '/ensembl/perlbrew/perls/perl-5.16.3/bin/starman';
 my $port       = $ENV{ENSEMBL_REST_PORT} || 8030;
-my $workers    = 5;
+my $workers    = $ENV{ENSEMBL_REST_WORKERS} || 8;
+my $max_requests=100000;
 my $access_log = "$root_dir/logs/access_log";
 my $error_log  = "$root_dir/logs/error_log";
 my $pid_file   = "$root_dir/logs/vectorbase_rest.pid";
@@ -21,12 +22,15 @@ Daemon::Control->new(
     lsb_stop     => '$syslog',
     lsb_sdesc    => 'EG REST server control',
     lsb_desc     => 'VectorBase REST server control',
+    stop_signals => [ qw(QUIT TERM TERM INT KILL) ],
     program      => $starman,
     program_args => [ 
       '--listen',     ":$port", 
       '--workers',    $workers, 
+      '--max-requests',$max_requests,
       '--access-log', $access_log, 
       '--error-log',  $error_log,     
+      '--preload-app',
       $psgi_file 
     ],
     pid_file     => $pid_file,
--- vectorbase_rest.psgi
+++ vectorbase_rest.psgi
@@ -1,17 +1,25 @@
 use strict;
 use warnings;
-use EnsEMBL::REST;
 use Plack::Builder;
+use EnsEMBL::REST;
 use Plack::Util;
 
-my $app = EnsEMBL::REST->apply_default_middlewares(EnsEMBL::REST->psgi_app);
+my $app = EnsEMBL::REST->psgi_app;
 
 builder {
 ## VB - handle /rest prefix  
   enable 'Rewrite', request => sub { s{^/rest(?=/|$)}{/}; return };
 ##  
-  enable 'DetectExtension';
-  enable 'EnsemblRestHeaders';
+  enable 'SizeLimit' => (
+      max_unshared_size_in_kb => (800 * 1024),    # 800MB per process (memory assigned just to the process)
+      check_every_n_requests => 10,
+      log_when_limits_exceeded => 1
+  );
+  enable "Plack::Middleware::ReverseProxy";
+  enable 'StackTrace';
+  enable 'Runtime';
+  enable "ContentLength";
   enable 'CrossOrigin', origins => '*', headers => '*', methods => ['GET','POST','OPTIONS'];
   $app;
 }
+
